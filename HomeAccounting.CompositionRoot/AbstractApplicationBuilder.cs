22ffb729e4787cd2b33f4b1bcda90f33691fae16
DESKTOP-F2OKRF2\HYSTOU <proburrell@gmai.com>
2021-06-04 18:31:34 +0300

    AbstractApplicationBuilder added

---
+++ HomeAccounting.CompositionRoot/AbstractApplicationBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace HomeAccounting.CompositionRoot
+{
+    public abstract class AbstractApplicationBuilder
+    {
+        protected readonly IServiceCollection _services;
+
+        /// <summary> Регистрация бизнес-логики </summary>
+        protected abstract void RegisterBusinessLogic();
+
+        /// <summary> Регистрация слоя данных </summary>
+        protected abstract void RegisterDataSource();
+
+        /// <summary> Регистрация инфраструктуры</summary>
+        protected abstract void RegisterInfrastructure();
+
+        public AbstractApplicationBuilder(IServiceCollection services)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+
+        public void Build()
+        {
+            RegisterInfrastructure();
+            RegisterDataSource();
+            RegisterBusinessLogic();
+        }
+    }
+}
